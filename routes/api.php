000cd9b5b51918c7bb3e7f1d6de1bbfe15661e1b
Dilip Waghmare <dilipwaghmarep15@gmail.com>
2026-04-18 10:21:07 +0530

    Implement AuthController with registration and login methods

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Public Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
